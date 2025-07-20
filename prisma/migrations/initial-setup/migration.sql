2e8645a850bc74e86bf9e54c07f858886685c662
Danny Hamshananth <hamshananth@gmail.com>
2025-07-20 21:34:23 +0530

    Added initial migration files

--- prisma/migrations/initial-setup/migration.sql
+++ prisma/migrations/initial-setup/migration.sql
@@ -1,63 +1,9 @@
 -- CreateTable
-CREATE TABLE "suppliers" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "phone" TEXT,
-    "email" TEXT,
-    "address" TEXT,
-    "homePage" TEXT,
-
-    CONSTRAINT "suppliers_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "supplier_contacts" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "jobTitle" TEXT,
-    "phone" TEXT,
-    "email" TEXT,
-    "accountNumber" TEXT,
-    "accountBank" TEXT,
-    "supplierId" INTEGER NOT NULL,
-
-    CONSTRAINT "supplier_contacts_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "employees" (
-    "id" SERIAL NOT NULL,
-    "photo" TEXT,
-    "name" TEXT NOT NULL,
-    "jobTitle" TEXT NOT NULL,
-    "phone" TEXT,
-    "email" TEXT,
-    "address" TEXT,
-    "haveProductAccess" BOOLEAN NOT NULL DEFAULT false,
-    "haveTransactionAccess" BOOLEAN NOT NULL DEFAULT false,
-    "haveStatisticAccess" BOOLEAN NOT NULL DEFAULT false,
-    "haveCompanyInfoAcess" BOOLEAN NOT NULL DEFAULT false,
-
-    CONSTRAINT "employees_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "customers" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "phone" TEXT,
-    "email" TEXT,
-    "address" TEXT,
-
-    CONSTRAINT "customers_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "product-categories" (
+CREATE TABLE "product_categories" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
 
-    CONSTRAINT "product-categories_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "product_categories_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -66,140 +12,13 @@ CREATE TABLE "products" (
     "name" TEXT NOT NULL,
     "photo" TEXT,
     "description" TEXT,
-    "suggestedUnitPrice" INTEGER NOT NULL,
-    "buyUnitPrice" INTEGER NOT NULL,
-    "inStock" INTEGER NOT NULL,
-    "onOrder" INTEGER NOT NULL DEFAULT 0,
-    "reorderLevel" INTEGER NOT NULL DEFAULT 0,
+    "suggestedUnitPrice" INTEGER,
+    "unitPrice" INTEGER NOT NULL,
     "productCategoryId" INTEGER,
 
     CONSTRAINT "products_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "orders" (
-    "id" SERIAL NOT NULL,
-    "customerId" TEXT,
-    "employeeId" INTEGER NOT NULL,
-    "orderedDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "requiredDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "shipmentDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "discount" INTEGER NOT NULL DEFAULT 0,
-    "rounding" INTEGER NOT NULL DEFAULT 0,
-
-    CONSTRAINT "orders_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "order_lines" (
-    "orderId" INTEGER NOT NULL,
-    "productId" INTEGER NOT NULL,
-    "buyUnitPrice" INTEGER NOT NULL,
-    "sellUnitPrice" INTEGER NOT NULL,
-    "quantity" INTEGER NOT NULL,
-
-    CONSTRAINT "order_lines_pkey" PRIMARY KEY ("orderId","productId")
-);
-
--- CreateTable
-CREATE TABLE "payment_types" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "payment_types_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "payments" (
-    "id" SERIAL NOT NULL,
-    "orderId" INTEGER NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "paymentTypeId" INTEGER NOT NULL,
-    "amount" INTEGER NOT NULL,
-
-    CONSTRAINT "payments_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "expense_types" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "expense_types_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "expenses" (
-    "id" SERIAL NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "expenseTypeId" INTEGER NOT NULL,
-    "description" TEXT NOT NULL,
-    "amount" INTEGER NOT NULL,
-    "supplierId" INTEGER,
-
-    CONSTRAINT "expenses_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "expense_order_lines" (
-    "expenseId" INTEGER NOT NULL,
-    "productId" INTEGER NOT NULL,
-    "unitPrice" DECIMAL(65,30) NOT NULL,
-    "quantity" INTEGER NOT NULL,
-
-    CONSTRAINT "expense_order_lines_pkey" PRIMARY KEY ("expenseId","productId")
-);
-
--- CreateTable
-CREATE TABLE "expense_payments" (
-    "id" SERIAL NOT NULL,
-    "expenseId" INTEGER NOT NULL,
-    "paymentDate" TIMESTAMP(3) NOT NULL,
-    "paymentTypeId" INTEGER NOT NULL,
-    "amount" INTEGER NOT NULL,
-
-    CONSTRAINT "expense_payments_pkey" PRIMARY KEY ("id")
-);
-
 -- AddForeignKey
-ALTER TABLE "supplier_contacts" ADD CONSTRAINT "supplier_contacts_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "suppliers"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "products" ADD CONSTRAINT "products_productCategoryId_fkey" FOREIGN KEY ("productCategoryId") REFERENCES "product-categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "orders" ADD CONSTRAINT "orders_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "customers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "orders" ADD CONSTRAINT "orders_employeeId_fkey" FOREIGN KEY ("employeeId") REFERENCES "employees"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "order_lines" ADD CONSTRAINT "order_lines_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "order_lines" ADD CONSTRAINT "order_lines_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "payments" ADD CONSTRAINT "payments_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "orders"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "payments" ADD CONSTRAINT "payments_paymentTypeId_fkey" FOREIGN KEY ("paymentTypeId") REFERENCES "payment_types"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "expenses" ADD CONSTRAINT "expenses_expenseTypeId_fkey" FOREIGN KEY ("expenseTypeId") REFERENCES "expense_types"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "expenses" ADD CONSTRAINT "expenses_supplierId_fkey" FOREIGN KEY ("supplierId") REFERENCES "suppliers"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "expense_order_lines" ADD CONSTRAINT "expense_order_lines_expenseId_fkey" FOREIGN KEY ("expenseId") REFERENCES "expenses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "expense_order_lines" ADD CONSTRAINT "expense_order_lines_productId_fkey" FOREIGN KEY ("productId") REFERENCES "products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "expense_payments" ADD CONSTRAINT "expense_payments_expenseId_fkey" FOREIGN KEY ("expenseId") REFERENCES "expenses"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "expense_payments" ADD CONSTRAINT "expense_payments_paymentTypeId_fkey" FOREIGN KEY ("paymentTypeId") REFERENCES "payment_types"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "products" ADD CONSTRAINT "products_productCategoryId_fkey" FOREIGN KEY ("productCategoryId") REFERENCES "product_categories"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
