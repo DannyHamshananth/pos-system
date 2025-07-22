55e850451fe582b8d08cca80e34c1aa2b91195e4
Danny Hamshananth <hamshananth@gmail.com>
2025-07-22 12:02:53 +0530

    Temporarily ignore some files to avoid errors - 2

--- lib/customer.ts
+++
@@ -1,6 +0,0 @@
-export async function getCustomers() {
-    const res = await fetch(`api/customers`, {
-        cache: 'no-store'
-    }).then((res) => res.json());
-    return res;
-}
--- lib/employee.ts
+++
@@ -1,6 +0,0 @@
-export async function getEmployees() {
-    const res = await fetch(`api/employees`, {
-        cache: 'no-store'
-    }).then((res) => res.json());
-    return res;
-}
