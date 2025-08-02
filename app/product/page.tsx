fd944b2b5f50db247ae6e985d45b82570e9d8bb4
Danny Hamshananth <hamshananth@gmail.com>
2025-08-02 15:45:11 +0530

    Added product popover with with fields UI

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -10,6 +10,10 @@ import { ProductList, columns } from "@/app/product/columns";
 import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/ui/dataTable";
 
+import { Popover, PopoverContent, PopoverTrigger, } from "@/components/ui/popover";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function Transaction() {
     const [refresh, setRefresh] = useState(true);
     const [data, setData] = useState<ProductList[]>([]);
@@ -28,7 +32,7 @@ export default function Transaction() {
     }, [refresh]);
 
     return (
-        <div className="py-6 px-10">
+        <div className="py-6 px-10 sm:px-20">
             <div className="flex flex-col sm:space-y-4">
                 <div className="mr-auto flex">
                     <h1 className="mr-auto font-bold text-2xl flex items-center">
@@ -40,10 +44,52 @@ export default function Transaction() {
                         </Button>
                     </div>
                 </div>
-                <div className="ml-auto flex sm:justify-normal sm:p-2 gap-5">
-                    <Button variant="outline" className="my-auto">
-                        <Plus className="w-4 h-4 mr-2" /> New product
-                    </Button>
+                <div className="ml-auto mt-10 flex sm:justify-normal sm:p-2 gap-5">
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <Button variant="outline">New Product</Button>
+                        </PopoverTrigger>
+                        <PopoverContent className="w-80">
+                            <div className="grid gap-4">
+                                <div className="space-y-2">
+                                    <h4 className="leading-none font-medium">Add new product</h4>
+                                    <p className="text-muted-foreground text-sm">
+                                        Add a new product with exising/new category.
+                                    </p>
+                                </div>
+                                <div className="grid gap-2">
+                                    <div className="grid grid-cols-3 items-center gap-4">
+                                        <Label htmlFor="width">Name</Label>
+                                        <Input
+                                            id="name"
+                                            className="col-span-2 h-8"
+                                        />
+                                    </div>
+                                    <div className="grid grid-cols-3 items-center gap-4">
+                                        <Label htmlFor="description">Description</Label>
+                                        <Input
+                                            id="description"
+                                            className="col-span-2 h-8"
+                                        />
+                                    </div>
+                                    <div className="grid grid-cols-3 items-center gap-4">
+                                        <Label htmlFor="unitPrice">Unit Price</Label>
+                                        <Input
+                                            id="unitPrice"
+                                            className="col-span-2 h-8"
+                                        />
+                                    </div>
+                                    <div className="grid grid-cols-3 items-center gap-4">
+                                        <Label htmlFor="maxHeight">Category</Label>
+                                        <Input
+                                            id="maxHeight"
+                                            className="col-span-2 h-8"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </PopoverContent>
+                    </Popover>
                     <Button className="my-auto">
                         <Download className="w-4 h-4 mr-2" /> Download
                     </Button>
