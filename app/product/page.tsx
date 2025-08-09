2c673abe899b5cad7420f0d2b7d1bd309e3b6eb2
Danny Hamshananth <hamshananth@gmail.com>
2025-08-09 16:25:20 +0530

    Add product feauture - Working - Semi

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { RefreshCw, Download, Plus } from "lucide-react";
+import { RefreshCw, Download, Plus, Loader2Icon, CheckIcon, ChevronsUpDownIcon } from "lucide-react";
 
 import { getProducts } from "@/lib/product";
 
@@ -14,9 +14,50 @@ import { Popover, PopoverContent, PopoverTrigger, } from "@/components/ui/popove
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { cn } from "@/lib/utils";
+
 export default function Transaction() {
+    const [open, setOpen] = React.useState(false); // State for category popover
     const [refresh, setRefresh] = useState(true);
     const [data, setData] = useState<ProductList[]>([]);
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState<string>("");
+    const [unitPrice, setUnitPrice] = useState<number>(0);
+    const [productCategoryId,setProductCategoryId] = useState<number>(0);
+
+    type Category = {id:Number,name:String};
+    
+    const [categories, setCategories] = useState<Category[]>([]);
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const add_product = async () => {
+        setIsVisible(true);
+        console.log(name, unitPrice, description, productCategoryId);
+        const res = await fetch(`api/products`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name, unitPrice, description, productCategoryId}),
+            cache: 'no-store',
+        });
+        if (res.status === 201) {
+            setName("");
+            setDescription("");
+            setProductCategoryId(0);
+            setUnitPrice(0);
+            setIsVisible(false);
+        }
+    }
 
     useEffect(() => {
         if (!refresh) {
@@ -28,6 +69,12 @@ export default function Transaction() {
             const data = await getProducts();
             setData(data);
             setRefresh(false);
+        })();
+
+        (async () => {
+            const response = await fetch('api/productCategories');
+            const data =  await response.json();
+            setCategories(data);
         })();
     }, [refresh]);
 
@@ -63,6 +110,8 @@ export default function Transaction() {
                                         <Input
                                             id="name"
                                             className="col-span-2 h-8"
+                                            value={name}
+                                            onChange={(e)=> setName(e.target.value)}
                                         />
                                     </div>
                                     <div className="grid grid-cols-3 items-center gap-4">
@@ -70,6 +119,8 @@ export default function Transaction() {
                                         <Input
                                             id="description"
                                             className="col-span-2 h-8"
+                                            value={description}
+                                            onChange={(e)=> setDescription(e.target.value)}
                                         />
                                     </div>
                                     <div className="grid grid-cols-3 items-center gap-4">
@@ -77,14 +128,65 @@ export default function Transaction() {
                                         <Input
                                             id="unitPrice"
                                             className="col-span-2 h-8"
+                                            value={isNaN(unitPrice)?"0":unitPrice?.toString()}
+                                            onChange={(e)=> setUnitPrice(parseInt(e.target.value))}
                                         />
                                     </div>
                                     <div className="grid grid-cols-3 items-center gap-4">
                                         <Label htmlFor="maxHeight">Category</Label>
-                                        <Input
-                                            id="maxHeight"
-                                            className="col-span-2 h-8"
-                                        />
+                                        <div className="col-span-2 h-8">
+                                            <Popover open={open} onOpenChange={setOpen}>
+                                                <PopoverTrigger asChild>
+                                                    <Button
+                                                        variant="outline"
+                                                        role="combobox"
+                                                        aria-expanded={open}
+                                                        className="justify-between w-full h-full"
+                                                    >
+                                                        {productCategoryId
+                                                            ? categories.find((category) => category.id === productCategoryId)?.name
+                                                            : "Select category..."}
+                                                        <ChevronsUpDownIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                                                    </Button>
+                                                </PopoverTrigger>
+                                                <PopoverContent className="w-[200px] p-0">
+                                                    <Command>
+                                                        <CommandInput placeholder="Search framework..." className=""/>
+                                                        <CommandList>
+                                                            <CommandEmpty>No framework found.</CommandEmpty>
+                                                            <CommandGroup>
+                                                                {categories.map((category) => (
+                                                                    <CommandItem
+                                                                        key={category.id.toString()}
+                                                                        value={category.id.toString()}
+                                                                        onSelect={(currentValue) => {
+                                                                            setProductCategoryId(parseInt(currentValue) == productCategoryId ? 0 : parseInt(currentValue))
+                                                                            setOpen(false)
+                                                                        }}
+                                                                    >
+                                                                        <CheckIcon
+                                                                            className={cn(
+                                                                                "mr-2 h-4 w-4",
+                                                                                productCategoryId === category.id ? "opacity-100" : "opacity-0"
+                                                                            )}
+                                                                        />
+                                                                        {category.name}
+                                                                    </CommandItem>
+                                                                ))}
+                                                            </CommandGroup>
+                                                        </CommandList>
+                                                    </Command>
+                                                </PopoverContent>
+                                            </Popover>
+                                        </div>
+                                    </div>
+                                    <div className="mt-2 grid grid-cols-4 items-center gap-4">
+                                        <Button onClick={add_product} className="col-start-2 col-end-4 gap-2">
+                                            {isVisible && (
+                                                <Loader2Icon className="animate-spin" />
+                                            )}
+                                            Save
+                                        </Button>
                                     </div>
                                 </div>
                             </div>
