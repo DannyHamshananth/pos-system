633e65f82c501a353d81439c84c81e12b980078b
Danny Hamshananth <hamshananth@gmail.com>
2025-08-09 16:22:12 +0530

    Product Category API - Clean code

--- app/api/productCategories/route.ts
+++ app/api/productCategories/route.ts
@@ -2,10 +2,10 @@ import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-    const products = await prisma.productCategory.findMany({
+    const categories = await prisma.productCategory.findMany({
         orderBy:{
             id:"asc"
         }
     });
-    return NextResponse.json(products);
+    return NextResponse.json(categories);
 }
